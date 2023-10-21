808905b2fa2a16d2be9dca13ae1d9e576bc1128a
EagleBlood <35221@student.pwsztar.edu.pl>
2023-10-21 16:43:50 +0200

    zad3

---
+++ zad3.js
@@ -0,0 +1,7 @@
+function concatArray(array1, array2) {
+    return array1.concat(array2);
+}
+var array1 = [1, 2.45, 3];
+var array2 = ['pies', 5, 'kot'];
+console.log("Zadanie 3\n\r");
+console.log("Połączone tablice: " + concatArray(array1, array2) + "\n\n\r");
