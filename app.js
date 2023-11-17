2a1d35aac49c86be626b829e3fafc31f89d9839e
EagleBlood <35221@student.pwsztar.edu.pl>
2023-11-17 13:11:37 +0100

    Lab5

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const service = require('./service');
 const express = require('express');
 const config = require('./config'); // dodając tę linię, trzeba pamiętać o stworzeniu pliku config.js
+const path = require('path');
 
 const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.set('view engine', 'html');
 app.engine('html', require('ejs').renderFile);
@@ -30,11 +33,47 @@ const dataChart2 = service.convertData(temp);
 const chart2 = JSON.stringify(dataChart2);
 
 app.get('/', (request, response) => {
-   response.render(__dirname + '/index.html', {subject: 'Technologie webowe w aplikacjach Internetu', chart1: JSON.stringify(chart1), chart2, products});
+   response.render(__dirname + '/public/index.html', {subject: 'Technologie webowe w aplikacjach Internetu', chart1: JSON.stringify(chart1), chart2, products});
+});
+
+app.use(express.static('public'));
+
+
+app.get('/:variant/:a/:b', (req, res) => {
+   const variant = req.params.variant;
+   const a = Number(req.params.a);
+   const b = Number(req.params.b);
+   let result;
+
+   switch (variant) {
+       case 'sum':
+           result = a + b;
+           break;
+       case 'sub':
+           result = a - b;
+           break;
+       case 'mul':
+           result = a * b;
+           break;
+       case 'div':
+           if (b !== 0) {
+               result = a / b;
+           } else {
+               res.status(400).send('Division by zero is not allowed');
+               return;
+           }
+           break;
+       default:
+           res.status(400).send('Invalid operation');
+           return;
+   }
+
+
+   res.render(path.join(__dirname, 'public', 'result.html'), { result: result });
 });
 
  
 
 app.listen(config.port, function () {
-   console.info(`Server is running at port 3000`);
+   console.info(`Server is running at port ${config.port}`);
 });
