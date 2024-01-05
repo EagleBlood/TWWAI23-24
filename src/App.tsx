764d83f721cec24e57354f5d6bfb29e24acc43b7
EagleBlood <35221@student.pwsztar.edu.pl>
2024-01-05 11:50:23 +0100

    lab8

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import './App.css'
 import Home from './components/home'
+import Posts from './components/posts'
+import AddPost from './components/addPosts'
 
 function App() {
   return (
     <>
       <Home/>
+       <AddPost></AddPost>
+       <Posts></Posts>
+
     </>
   )
 }
