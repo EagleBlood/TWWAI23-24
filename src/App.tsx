e11f37049bb75b27b7a9844386a6fcfecbc8d427
EagleBlood <35221@student.pwsztar.edu.pl>
2024-01-22 11:40:40 +0100

    chat addon

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,28 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Home from './components/home';
 import Charts from './components/charts';
+import ChatComponent from './components/chat';
 
 function App() {
-   return (
-       <Router>
-           <nav style={{ margin: 10 }}>
-               <Link to="/" style={{ padding: 5 }}>
-                   Home
-               </Link>
-               <Link to="/charts" style={{ padding: 5 }}>  {/* Add a link to the Charts page */}
-                   Charts
-               </Link>
-           </nav>
-           <Routes>
-               <Route path="/" element={<Home />} />
-               <Route path="/charts" element={<Charts />} /> {/* Add a route for the Charts page */}
-           </Routes>
-       </Router>
-   );
-}
+    return (
+        <Router>
+            <nav style={{ margin: 10 }}>
+                <Link to="/" style={{ padding: 5 }}>
+                    Home
+                </Link>
+                <Link to="/charts" style={{ padding: 5 }}>
+                    Charts
+                </Link>
+                <Link to="/chat" style={{ padding: 5 }}>
+                    Chat
+                </Link>
+            </nav>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/charts" element={<Charts />} />
+                <Route path="/chat" element={<ChatComponent />} />
+            </Routes>
+        </Router>
+    );
+ }
 export default App;
